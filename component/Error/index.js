5ca5996169ccc14b351851be5a2387797f6f4d68
noumankhandatu <noumankhandatu>
2022-08-23 12:47:39 +0500

    all pages converted

--- component/Error/index.js
+++ component/Error/index.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-// Import Error img
-import img1 from '../../assets/img/common/error.png'
+import React from "react";
+import Link from "next/link";
 
-// ErrorArea Area
 const ErrorMessages = () => {
-    return (
-        <>
-            <section id="error_area">
-                <div className="d-table">
-                    <div className="d-table-cell">
-                        <div className="container">
-                            <div className="error-img">
-                                <img src={img1} alt="Error Imgs"/>
-                                <h3>Page Not Found</h3>
-                                <Link to="/" className="btn btn-theme">Back To Home</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <section id="error_area">
+        <div className="d-table">
+          <div className="d-table-cell">
+            <div className="container">
+              <div className="error-img">
+                <img src={"https://picsum.photos/200/300"} alt="Error Imgs" />
+                <h3>Page Not Found</h3>
+                <Link href="/" className="btn btn-theme">
+                  Back To Home
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default ErrorMessages
+export default ErrorMessages;
