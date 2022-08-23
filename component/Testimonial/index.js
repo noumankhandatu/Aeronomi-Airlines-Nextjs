fbbe7eed08ea889c4f1e40b144f4f30609a04e26
noumankhandatu <noumankhandatu>
2022-08-23 12:39:32 +0500

    tes pages

--- component/Testimonial/index.js
+++ component/Testimonial/index.js
@@ -1,50 +1,49 @@
-import React from 'react'
-//Import ClientCard
-import ClientCard from '../Common/Client/ClientCard'
-//Import ClientsData
-import {ClientsData} from '../Common/Client/ClientData'
-//  OwlCarousel Slider Import
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import React from "react";
+import ClientCard from "../Common/Client/ClientCard";
+import { ClientsData } from "../Common/Client/ClientData";
+import Carousel from "react-elastic-carousel";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+];
 
 const TestimonialSlider = () => {
-    let responsive = {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 1,
-        },
-        960: {
-            items: 1,
-        },
-        1200: {
-            items: 1,
-        },
-    }
-    return (
-        <>
-            <section id="testimonial_homeTwo">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-10 offset-lg-1 col-md-12 col-sm-12 col-12">
-                            <div className="client_review_two ">
-                                <OwlCarousel className="owl-theme" responsive={responsive} autoplay={false}
-                                             autoplayHoverPause={true} autoplayTimeout={2500}
-                                             loop={true} margin={10} nav={false} dots={true}>
-                                    {ClientsData.map((data, index) => (
-                                        <ClientCard img={data.img} name={data.name} des={data.des} para={data.para}
-                                                    key={index}/>
-                                    ))}
-                                </OwlCarousel>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <section id="testimonial_homeTwo">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-10 offset-lg-1 col-md-12 col-sm-12 col-12">
+              <div className="client_review_two ">
+                <div className="carousel-wrapper">
+                  <Carousel
+                    itemPadding={[5]}
+                    enableAutoPlay={true}
+                    autoPlaySpeed={2000}
+                    showArrows={false}
+                    breakPoints={breakPoints}
+                  >
+                    {ClientsData.map((data, index) => (
+                      <ClientCard
+                        img={data.img}
+                        name={data.name}
+                        des={data.des}
+                        para={data.para}
+                        key={index}
+                      />
+                    ))}
+                  </Carousel>
                 </div>
-            </section>
-        </>
-    )
-}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default TestimonialSlider
+export default TestimonialSlider;
