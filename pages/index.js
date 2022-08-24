6bd1e5110b13d4bd3b04ee3947e1ca6f73222c54
noumankhandatu <noumankhandatu>
2022-08-24 11:38:40 +0500

    link removed

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ page }) {
       <div>
         <LandingPage />
       </div>
-      <script
+      {/* <script
         src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossOrigin="anonymous"
@@ -35,7 +35,7 @@ export default function Home({ page }) {
         src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossOrigin="anonymous"
-      ></script>
+      ></script> */}
     </div>
   );
 }
